3cce4adfefcd9fce872621b27eaddc2118503812
Gegreenpeaced <107betriebshof@gmail.com>
2023-03-14 20:00:36 +0100

    Initial Push

---
+++ WeatherDll/DllExp.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WeatherDll
+{
+    // SensorException für falsche Sensorwerte
+    class SensorException : Exception
+    {
+        public SensorException()
+        { }
+
+        public SensorException(string ExMsg) : base(ExMsg)
+        { }
+        public SensorException(string ExMsg, Exception inner) : base(ExMsg, inner)
+        { }
+        public override string HelpLink
+        {
+            get
+            {
+                return "Für mehr Informationen bitte Markus oder Julius fragen oder die Sensoren der Wetterstation überprüfen.";
+            }
+        }
+    }
+
+    class NetworkParsingException : Exception
+    {
+        public NetworkParsingException()
+        { }
+
+        public NetworkParsingException(string ExMsg) : base(ExMsg)
+        { }
+        public NetworkParsingException(string ExMsg, Exception inner) : base(ExMsg, inner)
+        { }
+        public override string HelpLink
+        {
+            get
+            {
+                return "Für mehr Informationen bitte Markus oder Julius fragen oder die Verbindung zur Wetterstation überprüfen.";
+            }
+        }
+    }
+}
---
+++ WeatherDll/mainPrgDll.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net;
+using System.Net.Http;
+using System.Net.Http.Headers;
+
+namespace WeatherDll
+{
+    // WeatherData Objekt für das Parsen an das Formular
+    public class WeatherData
+    {
+        float Temp;
+        float Press;
+        float Hum;
+        int UV;
+        int Light;
+    }
+
+    public class mainPrgDll
+    {
+        public WeatherData GetData()
+        { }
+    }
+}
